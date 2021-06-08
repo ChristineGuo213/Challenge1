3b2087942d01e00189af5b79f0b26b5ed34ffee5
ChristineGuo213 <christine.zz@hotmail.com>
2021-06-07 19:36:53 -0700

    corrected repo path using command line interface

--- Starter_Code/loan_analyzer.py
+++ Starter_Code/loan_analyzer.py
@@ -206,7 +206,7 @@ output_path = Path("inexpensive_loans.csv")
 
 
 
-# @TODO: Use the csv library and `csv.writer` to write the header row
+# Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
 with open(output_path, "w") as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
